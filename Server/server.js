88773f4edd50d6225a9bb5164d3b3d1f76847832
Dishit Shah <dishit.s@ahduni.edu.in>
2025-06-29 15:53:25 +0530

    Final 2.0 3.0 4.0 5.0

--- Server/server.js
+++ Server/server.js
@@ -202,10 +202,10 @@ passport.deserializeUser(async (id, done) => {
 
 passport.use(
   new GoogleStrategy(
-    {
+     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "https://completemoneymate.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
